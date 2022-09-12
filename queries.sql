ff2859673f6d3505cf19182b77e24b7c77b75b3b
Tushar <singhtushar348@gmail.com>
2022-09-12 22:34:54 +0100

    Write SQL to query the animals data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animals WHERE neutered AND escape_attempts<3;
+SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered;
+SELECT * FROM animals WHERE name != 'Gabumon';  
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
