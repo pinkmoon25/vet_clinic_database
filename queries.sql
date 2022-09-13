3892d1dae1a0d45286bca29f07943b45bbb90f3f
Tushar <singhtushar348@gmail.com>
2022-09-13 07:17:17 +0100

    Modify query for date of birth search

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon%';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT * FROM animals WHERE neutered AND escape_attempts<3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
