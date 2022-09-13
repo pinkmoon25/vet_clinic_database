c74d07165308bbed32abfd024a3b528ab658a24c
Tushar <singhtushar348@gmail.com>
2022-09-13 17:33:37 +0100

    Transaction query to update species and commit changes

--- queries.sql
+++ queries.sql
@@ -8,3 +8,20 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered;
 SELECT * FROM animals WHERE name != 'Gabumon';  
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/*transaction*/
+vet_clinic=# begin transaction;
+BEGIN
+vet_clinic=*# update animals set species = 'unspecified';
+UPDATE 11
+vet_clinic=*# rollback;
+ROLLBACK
+
+vet_clinic=# BEGIN;
+BEGIN
+vet_clinic=*# update animals set species = 'digimon' where name like '%mon';
+UPDATE 6
+vet_clinic=*# update animals set species = 'pokemon' where species IS NULL;
+UPDATE 5
+vet_clinic=*# commit;
+COMMIT
