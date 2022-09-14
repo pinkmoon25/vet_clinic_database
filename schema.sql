b774102b9567998e73feba704ba283045117375b
Tushar <singhtushar348@gmail.com>
2022-09-14 17:11:25 +0100

    Create owners and species table and modify animals table

--- schema.sql
+++ schema.sql
@@ -10,3 +10,15 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species VARCHAR;
+ALTER TABLE animals DROP column species;
+ALTER TABLE animals ADD PRIMARY KEY(id);
+alter table animals add species_id int;
+alter table animals add owner_id int;
+alter table animals add foreign key (species_id) references species(id);
+alter table animals add foreign key (owner_id) references owners(id);
+
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY, full_name VARCHAR, age INT);
+ALTER TABLE owners ADD PRIMARY KEY(id);
+
+CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR);
+ALTER TABLE species ADD PRIMARY KEY(id);
